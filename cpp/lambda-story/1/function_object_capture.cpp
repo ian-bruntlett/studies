5aad78e24720422f1211b98ec9c977a8d658cb3c
Ian Bruntlett <ian.bruntlett@gmail.com>
2025-11-25 14:21:21 +0000

    Adding chapter 1 function_object_capture (with 1 bugfix)

---
+++ cpp/lambda-story/1/function_object_capture.cpp
@@ -0,0 +1,28 @@
+// Example 1::4 function_object_capture.cpp
+#include <algorithm>
+#include <iostream>
+#include <string>
+#include <vector>
+
+struct PrinterEx {
+  PrinterEx(const std::string &str):  // was PrintEx
+    strText(str), numCalls(0) { }
+
+  void operator()(int x) {
+    std::cout << strText << x << '\n';
+    ++numCalls;
+  }
+
+  std::string strText;
+  int numCalls;
+};
+
+int main() {
+  std::vector<int> v;
+  v.push_back(1);
+  v.push_back(2);
+  const std::string introText("Elem: ");
+  const PrinterEx vis = std::for_each(v.begin(),v.end(),
+				      PrinterEx(introText));
+  std::cout << "num calls " << vis.numCalls << '\n';
+}
