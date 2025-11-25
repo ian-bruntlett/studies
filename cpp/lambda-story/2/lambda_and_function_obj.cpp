47bc0771639b5a6525260d564058d74bd59d6054
Ian Bruntlett <ian.bruntlett@gmail.com>
2025-11-25 15:43:52 +0000

    chapter 2 lambda_and_function_obj.cpp

---
+++ cpp/lambda-story/2/lambda_and_function_obj.cpp
@@ -0,0 +1,21 @@
+// lambda_and_function_obj.cpp
+#include <algorithm>
+#include <iostream>
+#include <vector>
+
+int main() {
+  struct {
+    void operator()(int x) const {
+      std::cout << x << '\n';
+	}
+  } someInstance;
+
+  const std::vector<int> v {1,2,3};
+  std::for_each(v.cbegin(), v.cend(), someInstance);
+  std::for_each(v.cbegin(), v.cend(), [] (int x) {
+    std::cout << x << '\n';
+  }
+    );
+}
+
+    
