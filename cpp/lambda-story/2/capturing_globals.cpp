99eaf7acdc5c45df4bc12e3fe690bfc05bf4f43a
Ian Bruntlett <ian.bruntlett@gmail.com>
2025-12-09 16:07:34 +0000

    chapter 2 capturing_globals.cpp capturing_static.cpp invocation_counter.cpp

---
+++ cpp/lambda-story/2/capturing_globals.cpp
@@ -0,0 +1,17 @@
+// 2.7 capturing_globals
+
+#include <iostream>
+
+int global = 10;
+
+int main() {
+  std::cout << global << '\n';
+  auto foo = [=]() mutable noexcept { ++global; };
+  foo();
+  std::cout << global << '\n';
+  const auto increaseGlobal= []() noexcept {++global;};
+  increaseGlobal();
+  const auto moreIncreaseGlobal = [global]() noexcept {++global;};
+  moreIncreaseGlobal();
+  std::cout << global << '\n';
+}
